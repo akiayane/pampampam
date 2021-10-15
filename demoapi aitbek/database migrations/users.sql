6437d9edf41dad556260e9b5700c9f74dd83124a
Olzhas <ol.ilyassov@gmail.com>
2021-10-16 00:45:56 +0600

    Delete demoapi aitbek directory

--- demoapi aitbek/database migrations/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-                       id int not null AUTO_INCREMENT,
-                       PRIMARY KEY (id),
-                       email varchar(255),
-                       pass varchar(255),
-                       name varchar(255),
-                       surname varchar(255),
-                       birthdate timestamp
-)
